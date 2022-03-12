ef527806be9c43d7a866ebb9062f66125ad8292b
andre <anfuerch.socials@gmail.com>
2022-03-12 21:37:24 +0100

    Add display functionality.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
+const display = document.getElementById("display");
+let highlightedOperator;
+let firstValue = 0;
+let secondValue = "";
+let currentOperator;
+let currentOperatorDisplay;
+
+
+
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener("click", selectOperator)
+});
+
+digitButtons.forEach(digitButton => {
+    digitButton.addEventListener("click", selectDigit)
+});
+
 function operate(operator, number1, number2) {
     return operator(number1, number2);
 }
@@ -17,4 +36,61 @@ function multiply(factor1, factor2) {
 
 function divide(dividend, divisor) {
     return dividend / divisor;
+}
+
+function selectDigit() {
+
+    let buttonValue = parseInt(this.innerText);
+
+    if (!currentOperator) {
+        firstValue *= 10;
+        firstValue += buttonValue;
+    }
+    else {
+        console.log("works")
+        secondValue *= 10;
+        secondValue += buttonValue;
+    }
+
+
+    updateDisplay();
+
+}
+
+function updateDisplay() {
+    if (!currentOperator)
+        display.innerText = `${firstValue}`;
+    else {
+        display.innerText = `${firstValue} ${currentOperatorDisplay} ${secondValue}`
+    }
+}
+
+function selectOperator() {
+
+    if (highlightedOperator) {
+        highlightedOperator.classList.remove("highlighted");
+    }
+    currentOperator = this.id;
+    highlightedOperator = this;
+    highlightedOperator.classList.add("highlighted");
+
+    setCurrentOperatorDisplay();
+
+    updateDisplay();
+
+
+
+}
+
+function setCurrentOperatorDisplay() {
+    switch (currentOperator) {
+        case "add": currentOperatorDisplay = "+";
+            break;
+        case "subtract": currentOperatorDisplay = "-";
+            break;
+        case "multiply": currentOperatorDisplay = "*";
+            break;
+        case "divide": currentOperatorDisplay = "/";
+            break;
+    }
 }
